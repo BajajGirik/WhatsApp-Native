57b41d2a0326f4a626bc543ad7568545db1ca83e
Girik Bajaj <bajajgirik2010@gmail.com>
2021-07-01 20:13:48 +0530

    Styling Login page

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, View, Platform } from 'react-native'
 import { Input, Image, Button } from 'react-native-elements';
 import Logo from '../assets/Logo.png'
+import { Zocial, Entypo } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { nodeName } from 'min-document';
+
+
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
     return (
-        <KeyboardAvoidingView style={styles.container}>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
             <Image
                 source={{ uri: Logo }}
                 style={{ width: 200, height: 200 }}
             />
             <Input
+                inputStyle={styles.input}
+                containerStyle={styles.inputcontainer}
                 placeholder="Email"
-                onChangeText={value => this.setState({ comment: value })}
+                leftIcon={<Zocial name="email" size={24} color="white" /> }
+                onChangeText={value => setEmail(value)}
+            />
+            <Input
+                inputStyle={styles.input}
+                containerStyle={styles.inputcontainer}
+                placeholder="Password"
+                secureTextEntry
+                leftIcon={<Entypo name="lock" size={24} color="white" /> }
+                onChangeText={value => setPass(value)}
             />
             <View style={{ height: 100 }}></View>
         </KeyboardAvoidingView>
@@ -23,8 +44,19 @@ export default LoginScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        display: "flex",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "black"
+    },
+
+    inputcontainer: {
+        width: 400,
+        margin: 5,
+    },
+
+    input: {
+        marginLeft: 7,
+        color: "white",
     }
-})
+});
