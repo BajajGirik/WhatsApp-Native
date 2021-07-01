84c41a87d7fe639e5ced497865dced25357fd741
Girik Bajaj <bajajgirik2010@gmail.com>
2021-07-01 22:10:30 +0530

    Building register screen

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -3,9 +3,32 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const RegisterScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text> Register here</Text>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            <Image
+                source={{ uri: Logo }}
+                style={{ width: 200, height: 200 }}
+            />
+            <Input
+                inputStyle={styles.input}
+                containerStyle={styles.inputcontainer}
+                placeholder="Email"
+                leftIcon={<Zocial name="email" size={24} color="white" /> }
+                onChangeText={value => setEmail(value)}
+            />
+            <Input
+                inputStyle={styles.input}
+                containerStyle={styles.inputcontainer}
+                placeholder="Password"
+                secureTextEntry
+                leftIcon={<Entypo name="lock" size={24} color="white" /> }
+                onChangeText={value => setPass(value)}
+            />
+
+            <View style={{ height: 100 }}></View>
+        </KeyboardAvoidingView>
     )
 }
 
