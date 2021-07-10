585c79b3de61b5477312cfec1e91c582d73059a9
Girik Bajaj <bajajgirik2010@gmail.com>
2021-07-10 13:21:24 +0530

    Added speeddial

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React, { useLayoutEffect, useEffect, useState } from 'react';
 import { ScrollView, SafeAreaView, View, TouchableOpacity} from 'react-native';
-import { Avatar } from 'react-native-elements';
+import { Avatar, SpeedDial } from 'react-native-elements';
 import { FontAwesome } from '@expo/vector-icons';
 import { auth, db } from '../firebase';
 import ChatList from '../components/ChatList';
 
 const HomeScreen = ({ navigation }) => {
     const [chats, setChats] = useState([]);
+    const [open, setOpen] = useState(true);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -14,9 +15,10 @@ const HomeScreen = ({ navigation }) => {
             headerTitleAlign: "left",
             headerRight: () => (
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <TouchableOpacity onPress={() => navigation.navigate("AddChat") }>
+                    {/* <TouchableOpacity onPress={() => navigation.navigate("AddChat") }>
                         <FontAwesome name="pencil" size={24} color="black" />
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
+
                     <Avatar
                         rounded
                         title={auth.currentUser.displayName[0]}
@@ -51,8 +53,8 @@ const HomeScreen = ({ navigation }) => {
     },[])
 
     return (
-        <SafeAreaView>
-            <ScrollView style={{ height: "100%" }}>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView style={{ flexGrow: 1 }}>
                 {chats.map((chat) => (
                     <TouchableOpacity key={chat.id} activeOpacity={0.8}>
                         <ChatList id={chat.id} data={chat.data} goToChat={goToChat}/>
@@ -60,6 +62,26 @@ const HomeScreen = ({ navigation }) => {
                 )
                 )}
             </ScrollView>
+
+            <SpeedDial
+                size="large"
+                isOpen={open}
+                icon={{ name: 'add', color: '#fff' }}
+                openIcon={{ name: 'close', color: '#fff' }}
+                onOpen={() => setOpen(!open)}
+                onClose={() => setOpen(!open)}
+            >
+                <SpeedDial.Action
+                    icon={{ name: 'chat', color: '#fff' }}
+                    title="Start Personal Chat"
+                    onPress={() => console.log('Add Something')}
+                />
+                <SpeedDial.Action
+                    icon={{ name: 'group', color: '#fff' }}
+                    title="Make A Group"
+                    onPress={() => console.log('Delete Something')}
+                />
+            </SpeedDial>
         </SafeAreaView>
     )
 }
